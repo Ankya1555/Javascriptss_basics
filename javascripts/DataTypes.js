2b04c90ed6a6d3f90f8e7a1dc97c035327c30390
Aniket Vilas Shendge <149756494+Ankya1555@users.noreply.github.com>
2025-01-15 13:34:52 +0000

    conversion and operations

---
+++ javascripts/DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newr versions
+
+// alert(3+3)  // we are using nodejs not a browser  
+
+console.log(3+3) // code readabily shoud be high 
+  
+  console.log("aniket")
+  let name = "anii"  //string
+let age = 24 //int
+let isLoggedIn = false
+
+
+// number => 2 to power 53 
+// bigint 
+// string '',"" use 
+//bolean  => truee or false 
+// null => standalone value
+// undifined  => when u create one vairable not assign value or use any where 
+// symbol => define idual componts or unique 
+
+
+// obeject 
+console.log(typeof undefined ); // undefined 
+console.log(typeof null );  // object 
+
+
---
+++ javascripts/Vriables.js
@@ -0,0 +1,20 @@
+const accountId =12456
+let accountEmail = "aniketshendge50@gmail.com"
+var accountPassword = "12345"
+accountcity = "Pune"
+let accountState = null
+
+
+
+// accountId = 2 not allowed //
+
+accountEmail ="anikets50@gmail.com"
+accountPassword = "12345678"
+accountCity = "Pune"
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPassword , accountState])
+
+/*
+ prefer not var use
+ because of issue in block scope and functional scope 
+*/
