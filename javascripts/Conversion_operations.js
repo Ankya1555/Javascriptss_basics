f9cb62ebcec456215b806629da4d94f562befec1
Aniket Vilas Shendge <149756494+Ankya1555@users.noreply.github.com>
2025-01-16 13:49:24 +0000

    data types

---
+++ javascripts/Conversion_operations.js
@@ -0,0 +1,54 @@
+"use strict"; //treat all js code as newr versions
+
+// alert(3+3)  // we are using nodejs not a browser  
+
+console.log(3+3) // code readabily shoud be high 
+  
+  console.log("aniket")
+  let name = "anii"  //string
+let age = 24 //int
+let isLoggedIn = false
+
+
+// number => 2 to power 53 
+// bigint 
+// string '',"" use 
+//bolean  => truee or false 
+// null => standalone value
+// undifined  => when u create one vairable not assign value or use any where 
+// symbol => define idual componts or unique 
+
+
+// obeject 
+console.log(typeof undefined ); // undefined 
+console.log(typeof null );  // object 
+
+
+// *******************Operations***************//
+let value =3
+let negValue = -value
+// console.log(negValue);
+ console.log(2+2);
+ console.log(3-2);
+ console.log(3*2);
+ console.log(20/2);
+ console.log(2%3);
+
+
+ let str1 =  "aniket"
+ let str2 = " shendge"
+ let str3 = str1+ str2
+ console.log(str3);
+
+
+ console.log("1"+2);
+ console.log(1+"2");
+ console.log(2+2+"1");
+ console.log((3 + 4)* 5 % 3 );
+
+
+//boolean value checked 
+
+ console.log(+true);
+ console.log(true);
+
