b6ba3638cacb143f7fd122c1549fe9b8c808086c
Aniket Vilas Shendge <149756494+Ankya1555@users.noreply.github.com>
2025-01-17 12:52:53 +0000

    string

--- javascripts/datatypes_sumary.js
+++ javascripts/datatypes_sumary.js
@@ -4,16 +4,16 @@
 // Symbol ,Bigint 
 // it alway call by value data types 
 
-const score = 100 
-const scoreValue = 100.3 
-const isLoggedIn = false
-const outsideTemp =null
-let userEmail;
-const id = Symbol("123")
-const anotherId = Symbol('123')
+// const score = 100 
+// const scoreValue = 100.3 
+// const isLoggedIn = false
+// const outsideTemp =null
+// let userEmail;
+// const id = Symbol("123")
+// const anotherId = Symbol('123')
  
 
-const bigNumber = 234792374n
+// const bigNumber = 234792374n
 
 
 
@@ -22,24 +22,44 @@ const bigNumber = 234792374n
  // Array , objects , Functions 
 
 
- const heros = ["shaktiman", "naagraj", "dogaa"   ]
+//  const heros = ["shaktiman", "naagraj", "dogaa"   ]
 
-let mybj =  {
-    name : "aniket"
-   // age : 24 ,
+// let mybj =  {
+//     name : "aniket"
+//    // age : 24 ,
 
- }
+//  }
 
- const myFunctions =  function(){
-    console.log("hello woorld");
+//  const myFunctions =  function(){
+//     console.log("hello woorld");
 
- }
- console.log(typeof outsideTemp)
+//  }
+//  console.log(typeof outsideTemp)
 
  // javascript dyanamic type language or staticaly type lngause hai ..?
  //  ==>  JavaScript is a dynamically typed language, not a statically typed language.
- let x = 10; // x is a number
-x = "Hello"; // Now x is a string
+ //let x = 10; // x is a number
+//x = "Hello"; // Now x is a string
 // for refference notes
 
 //  https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// stack (primitive types ) , heap <non - primitive data types > 
+
+let myYoutubename = "aniketshendge50"
+let anothername = myYoutubename
+console.log(anothername);
+
+let userOne = {
+   email: "aniketshendge50.gmail.com",
+   upi:   "aniekt@ubl"
+
+}
+let userTwo = userOne
+userTwo.email = "aniketshendge004@gmail.com"
+ console.log(userOne.email);
+ console.log(userTwo.email);
