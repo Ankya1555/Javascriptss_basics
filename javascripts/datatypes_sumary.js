27906520e4e014a319d01ea24a721504a926e523
Aniket Vilas Shendge <149756494+Ankya1555@users.noreply.github.com>
2025-01-17 12:36:43 +0000

    dat atypes summary

--- javascripts/datatypes_sumary.js
+++ javascripts/datatypes_sumary.js
@@ -13,13 +13,33 @@ const id = Symbol("123")
 const anotherId = Symbol('123')
  
 
+const bigNumber = 234792374n
+
+
 
  // Reffernce types (non pri,itive Data type)
 
  // Array , objects , Functions 
 
 
+ const heros = ["shaktiman", "naagraj", "dogaa"   ]
+
+let mybj =  {
+    name : "aniket"
+   // age : 24 ,
+
+ }
+
+ const myFunctions =  function(){
+    console.log("hello woorld");
+
+ }
+ console.log(typeof outsideTemp)
+
  // javascript dyanamic type language or staticaly type lngause hai ..?
  //  ==>  JavaScript is a dynamically typed language, not a statically typed language.
  let x = 10; // x is a number
 x = "Hello"; // Now x is a string
+// for refference notes
+
+//  https://262.ecma-international.org/5.1/#sec-11.4.3
