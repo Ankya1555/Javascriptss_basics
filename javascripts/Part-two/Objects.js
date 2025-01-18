d0d86df7b0b756a26e185de37eef84dff426eec9
Aniket Vilas Shendge <149756494+Ankya1555@users.noreply.github.com>
2025-01-18 14:25:42 +0000

    Objects create and strore

---
+++ javascripts/Part-two/Objects.js
@@ -0,0 +1,19 @@
+//singleton
+
+
+// object literals
+//  { } object 
+const JsUser = {
+    name : "aniket",
+    // "full name ": "aniket shendge"
+    age : 24,
+    location: "pune",
+    email: "aniketshendge.com",
+    isLoggedIn : false,
+    lastLoginDays:['Monday',"sunday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser.isLoggedIn);
+console.log(JsUser.name)
+console.log(JsUser.location);
