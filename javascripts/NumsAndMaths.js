9e1fcf4c84c291ed1704c4b89411f2e663bca6b2
Aniket Vilas Shendge <149756494+Ankya1555@users.noreply.github.com>
2025-01-18 12:05:28 +0000

    maths and num

---
+++ javascripts/NumsAndMaths.js
@@ -0,0 +1,36 @@
+const score = 400
+
+const balence = new Number(100)
+console.log(balence);
+console.log(score);
+
+console.log(balence.toString().length);
+console.log(balence.toFixed(2));
+
+const otherNumber = 1223.78555
+console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+ //**************-----Maths----*****************
+
+ console.log(Math);
+
+ console.log(Math.abs(4));
+ console.log(Math.abs(-4));
+ console.log(Math.round(5.6));
+ console.log(Math.ceil(4.8));
+ console.log(Math.min(11,12,123,22,3,4,1,3,55,));
+ console.log(Math.floor(Math.random()*10)+1);
+
+ const min = 100
+ const max = 200
+
+ console.log(Math.floor(Math.random()* (max- min +1))+min)
+
+
+ //https://www.youtube.com/watch?v=_KqpeDc47Ro&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=12
+ // watch this videpo for reffrencee  
+ 
